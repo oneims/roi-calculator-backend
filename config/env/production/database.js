de022c2c30a03399edc988a4a2f185fb0a1f4eb8
bokhariLovesYou <parasbokhari@gmail.com>
2023-02-12 20:26:38 -0600

    updates for railway

--- config/env/production/database.js
+++ config/env/production/database.js
@@ -1,21 +1,33 @@
+const parse = require("pg-connection-string").parse;
+const config = parse(process.env.DATABASE_URL);
+
 module.exports = ({ env }) => ({
-  connection: {
-    client: "postgres",
-    connection: {
-      host: env("PGHOST", "127.0.0.1"),
-      port: env.int("PGPORT", 5432),
-      database: env("PGDATABASE", "strapi"),
-      user: env("PGUSER", "strapi"),
-      password: env("PGPASSWORD", "password"),
-      ssl: env.bool(true),
-    },
-    pool: {
-      min: 0,
-      max: 10,
-      idleTimeoutMillis: 30000,
-      createTimeoutMillis: 30000,
-      acquireTimeoutMillis: 60000,
-      propagateCreateError: false,
+  defaultConnection: "default",
+  connections: {
+    default: {
+      connector: "bookshelf",
+      settings: {
+        client: "postgres",
+        host: config.host,
+        port: config.port,
+        database: config.database,
+        username: config.user,
+        password: config.password,
+        ssl: {
+          rejectUnauthorized: false,
+        },
+      },
+      pool: {
+        min: 0,
+        max: 10,
+        idleTimeoutMillis: 30000,
+        createTimeoutMillis: 30000,
+        acquireTimeoutMillis: 60000,
+        propagateCreateError: false,
+      },
+      options: {
+        ssl: true,
+      },
     },
   },
 });
--- config/env/production/server.js
+++ config/env/production/server.js
@@ -1,3 +1,7 @@
 module.exports = ({ env }) => ({
-  url: env("https://radiant-dawn-48942.herokuapp.com/"),
+  host: env("HOST", "0.0.0.0"),
+  port: env.int("PORT", 1337),
+  app: {
+    keys: env.array("APP_KEYS"),
+  },
 });
